8cb779f444a9760ff04ec344d18d7ff3b31929d1
haoenlu <haoenlu.07@gmail.com>
2024-04-10 02:36:43 -0400

    update

--- models.py
+++ models.py
@@ -93,10 +93,30 @@ class EEG_Generator(nn.Module):
             nn.LeakyReLU(0.2)
         )
 
-        self.block = nn.Sequential(
+        self.block1 = nn.Sequential(
             self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
             self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
-            )
+        )
+        self.block2 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
+        )
+        self.block3 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
+        )
+        self.block4 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
+        )
+        self.block5 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
+        )
+        self.block6 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=True),
+            self.make_conv1d_block(hidden_dim,hidden_dim,upsample=False)
+        )
 
 
         self.last = nn.Conv1d(hidden_dim,out_features,kernel_size=5,padding="same")
@@ -117,8 +137,12 @@ class EEG_Generator(nn.Module):
     def forward(self,noise):
         out = self.fc1(noise)
         out = torch.reshape(out,(-1,self.hidden_dim,self.sequence_len//64))
-        for i in range(6):
-            out = self.block(out)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.block5(out)
+        out = self.block6(out)
         out = self.last(out)
         out = torch.reshape(out,(-1,self.out_features,self.sequence_len))
         
@@ -129,11 +153,30 @@ class EEG_Discriminator(nn.Module):
         super().__init__()
         self.first = nn.Conv1d(in_features,hidden_dim,3,padding="same")
 
-        self.block = nn.Sequential(
+        self.block1 = nn.Sequential(
             self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
             self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
         )
-
+        self.block2 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
+        )
+        self.block3 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
+        )
+        self.block4 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
+        )
+        self.block5 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
+        )
+        self.block6 = nn.Sequential(
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=False),
+            self.make_conv1d_block(hidden_dim,hidden_dim,downsample=True)
+        )
 
         self.last = nn.Linear(hidden_dim*sequence_len//64,1)
         self.sigmoid = nn.Sigmoid()
@@ -150,8 +193,13 @@ class EEG_Discriminator(nn.Module):
     
     def forward(self,x):
         _x = self.first(x)
-        for i in range(6):
-            _x = self.block(_x)
+        _x = self.block1(_x)
+        _x = self.block2(_x)
+        _x = self.block3(_x)
+        _x = self.block4(_x)
+        _x = self.block5(_x)
+        _x = self.block6(_x)
+        _x = torch.flatten(_x,start_dim=1)
         _x = self.last(_x)
         _x = self.sigmoid(_x)
         return _x
